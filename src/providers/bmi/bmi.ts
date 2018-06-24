b33252f7b471d6c1cf8722bebad3cf4efd7580ab
sKhcodes <alifeforwards@hotmail.com>
2018-06-24 12:54:28 +0100

    classify BMI

--- src/providers/bmi/bmi.ts
+++ src/providers/bmi/bmi.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class BmiProvider {
 
-  calculateBmi (height:number, weight: number) {
+  calculateBmi (height: number, weight: number) {
     return weight / height / height;
   }
   }
